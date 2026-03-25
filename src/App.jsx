66b1a917b432577ce0a97744eed7a9fef27b3a86
joouseong <joowsooy@gmail.com>
2026-03-25 11:03:42 +0900

    0325 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+
+function Profile () {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default function App() {
+  return (
+    <>
+      <img className="button-icon" src={reactLogo} alt="" />
+    </>
+  )
+}
