484e9c6d71f7e1aacb068e772cf8fb529f348aec
joouseong <joowsooy@gmail.com>
2026-04-29 12:50:50 +0900

    0429 2nd commit

--- src/components/ButtonCom/ButtonCom.jsx
+++ src/components/ButtonCom/ButtonCom.jsx
@@ -1,12 +1,16 @@
 import style from "./ButtonCom.module.css"
 
+function handleClick() {
+    alert("버튼 클릭")
+}
+
 export default function ButtonCom() {
     return(
         <>
             <h1 className={style.title}>ButtonCom 컴포넌트</h1>
             <nav className={style.navBar}>
-                <button className={style.myButton}>버튼1</button>
-                <button className={style.myButton}>버튼2</button>
+                <button onClick={handleClick} className={style.myButton}>버튼1</button>
+                <button onClick={handleClick} className={style.myButton}>버튼2</button>
             </nav>
         </>
     )
